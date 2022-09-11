bf85b0c4f903f19f492a8c28c704016b59fca70f
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-09-11 13:37:26 -0600

    Added default 404 route and default error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Application } from 'express';
+import express, { NextFunction } from 'express';
+import type { Application, Request, Response } from 'express';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -9,10 +10,10 @@ import { COOKIE_SECRET, CORS_ORIGIN } from './utils/secrets';
 import albumRouter from './routes/album';
 import artistRouter from './routes/artist';
 import authRouter from './routes/auth';
-import indexRouter from './routes';
 import listRouter from './routes/list';
 import profileRouter from './routes/profile';
 import searchRouter from './routes/search';
+import logger from './utils/logger';
 
 initializePassport();
 
@@ -30,12 +31,28 @@ app.use(
 );
 app.use(morgan('dev'));
 
-app.use('/', indexRouter);
 app.use('/album', albumRouter);
 app.use('/artist', artistRouter);
 app.use('/auth', authRouter);
 app.use('/list', listRouter);
 app.use('/profile', profileRouter);
 app.use('/search', searchRouter);
+
+// Catch-all 404 route
+app.use('*', (req: Request, res: Response) => {
+  return res.status(404).json({ msg: 'not found' });
+});
+
+// Default error handler
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err.name === 'CastError') {
+    logger.warn(`invalid ObjectID passed to route: ${req.url}`);
+    return res.status(400).json({ msg: 'invalid id' });
+  }
+
+  logger.error(new Error(err).stack);
+  return res.status(500).json({ msg: 'server error' });
+});
 
 export default app;
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const router: Router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-  return res.json({ msg: 'hello' });
-});
-
-export default router;
